0cfe4de4c5ae43363838ae61774f2817d59e8cc3
robertmay2003 <robertmay2003@gmail.com>
2018-11-22 03:50:04 -0500

    Set up pug

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const Agent = require('../scripts/agent');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-	res.render('index');
+	res.render('index', {multiplayer: true});
+});
+
+router.get('/computer', function(req, res) {
+	res.render('index', {multiplayer: false})
 });
 
 router.post('/move', function (req, res) {
