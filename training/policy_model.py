424a9992720466ba1be4ac738fc10cb37bc1a598
zankner <zackankner@gmail.com>
2018-11-23 22:34:58 -0500

    Finished Policy Network

--- training/policy_model.py
+++ training/policy_model.py
@@ -6,10 +6,8 @@ class PolicyNetwork(object):
 	def __init__(self, hparams, sess):
 		self.s = sess
 
-
-	def agent(self, hparams, sess):
-		#build the graph
-		self._input = tf.placeholder(tf.float32,
+        #build the graph
+        self._input = tf.placeholder(tf.float32,
             shape=[None, hparams['input_size']])
 
         hidden1 = tf.contrib.layers.fully_connected(
@@ -42,7 +40,6 @@ class PolicyNetwork(object):
         #update
         optimizer = tf.train.RMSPropOptimizer(hparams['learning_rate'])
 
-
     def act(self,state):
     	#get an action through sampling
     	return self.s.run(self.train)
@@ -55,7 +52,75 @@ class PolicyNetwork(object):
     	#Run an episode
 
     	observation, reward, done = ____, 0, False
-    	obs, acts, rews, 
+    	obs, acts, rews = [], [], []
+
+        while not done:
+            obs.append(observation)
+
+            action = agent.act(observation)
+            reward = #reward function
+            done = #get wheter done
+
+            acts.append(action)
+            rews.append(reward)
+
+        return obs, acts, rews
+
+
+    def process_reward(rews):
+        #Rewards -> Advantages for one episode
+        return [len(rews)] * len(rews)
+
+    def main():
+
+        #hyper params
+        hparams = {
+            'input_size': #shape of the game,
+            'hidden_size': 36,
+            'num_actions': 3,
+            'learning_rate': 0.1}
+
+        #enviorment params
+        eparams = {
+            'num_batches': 40,
+            'ep_per_batch': 10
+        }
+
+
+        with tf.Graph().as_default(), tf.Session() as sess:
+
+            agent = PolicyNetwork(hparams, sess)
+
+            sess.run(tf.initialize_all_variables())
+
+            for batch in range(eparams['num_batches']):
+
+                print '=====\nBATCH {}\n===='.format(batch)
+                
+                b_obs, b_acts, b_rews = [], [], []       
+
+                for episode in range(eparams['ep_per_batch'])         
+
+                    obs, acts, rews = policy_rollout()#still have to decide what to do
+
+                    print 'Episode steps: {}'.format(len(obs))
+
+                    b_obs.extend(obs)
+                    b_acts.extend(acts)
+
+                    advantages = process_reward(rews)
+                    b_rews.extend(advantages)
+
+                #update policy
+                #normalize rewards; don't divide by 0
+                b_rews = (b_rews - np.mean(b_rews)) / (np.std(b_rews) + 1e-10)
+
+                agent.train_step(b_obs, b_acts, b_rews)
+
+if __name__ == '__main__':
+    main()
+
+
 
 
 
