af7e8643f7c90a0fa13ca5040ec23d57a36435ac
Ben Botvinick <benbotvinick@gmail.com>
2018-11-24 02:50:01 -0500

    add theoretical train method, include tensorflow

--- models/game_manager.js
+++ models/game_manager.js
@@ -1,3 +1,6 @@
+const tf = require('@tensorflow/tfjs');
+require('@tensorflow/tfjs-node');
+
 const Tile = require('./tile');
 const Grid = require('./grid');
 const Agent = require('./agent');
@@ -252,5 +255,39 @@ module.exports = class GameManager {
 
 	getState() {
 		return this.grid.flatten().map(x => x == null ? 0 : Math.log2(x.value));
+	}
+
+	train(games) {
+		let data = {};
+
+		for (let i = 0; i < games; i++) {
+			do {
+				// initialize variables
+				const stateString = JSON.stringify(this.getState());
+				const cumulativeReward1 = this.getCumulativeReward();
+
+				// get current weights for state
+				let weights;
+				if (!data[stateString]) weights = tf.ones([4]).softmax().dataSync();
+				else weights = data[stateString];
+
+				// perform action
+				const action = tf.multinomial(tf.log(weights), 1).dataSync()[0];
+				this.move(action);
+
+				// get reward of action
+				const cumulativeReward2 = this.getCumulativeReward();
+				const reward = cumulativeReward2 - cumulativeReward1;
+
+				// update weights
+				weights[action] += reward;
+				data[stateString] = tf.tensor1d(weights).softmax().dataSync();
+			} while (!this.isGameTerminated());
+
+			console.log('Cumulative reward: ' + this.getCumulativeReward());
+			this.restart();
+		}
+
+		console.log(data);
 	}
 };
